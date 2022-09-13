5f51b57356cafbcda02c3b3fc0a3fa2c50a98291
Rehan Razaq <58367730+rehan509@users.noreply.github.com>
2022-09-13 16:23:24 +0500

    vercel checking

--- src/App.js
+++ src/App.js
@@ -1,54 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 import icondice from "./icon-dice.svg";
-import logdivider  from "./pattern-divider-desktop.svg";
-import { useState,useEffect } from 'react';
+import logdivider from "./pattern-divider-desktop.svg";
+import { useState, useEffect } from "react";
 function App() {
-  const [text, setText] = useState([])
+  const [text, setText] = useState([]);
 
   const fetchAdvice = async () => {
-    const res = await fetch("https://api.adviceslip.com/advice")
-    const data = await res.json()
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
 
-    console.log(data)
+    console.log(data);
 
-    setText(data.slip)
-  }   
+    setText(data.slip);
+  };
 
   useEffect(() => {
-    fetchAdvice()
-  }, [])
+    fetchAdvice();
+  }, []);
 
-
-
- 
   return (
     <>
-    
-    <div className='bg-[#1F2632] md:p-[10rem] md:h-[50rem] text-center '>
+      <div className="bg-[#1F2632] md:p-[10rem] md:h-[50rem] text-center ">
+        <div className="text-white text-center pt-[10%] pb-[50%] md:pb-[0%] bg-[rgb(50,58,73)] rounded-sm">
+          <h1 className="text-dice-color text-[1.25rem] title my-4 pt-5">
+            Advice ${text.id}{" "}
+          </h1>
+          <h2 className=" text-[2rem] advice-text my-4">{text.advice}</h2>
 
-         <div className='text-white text-center pt-[10%] pb-[50%] md:pb-[0%] bg-[rgb(50,58,73)] rounded-sm'>
-          <h1 className='text-dice-color text-[1.25rem] title my-4 pt-5'>Advice  #{text.id} </h1>
-          <h2 className=' text-[2rem] advice-text my-4'>{text.advice}</h2>
+          <div className="flex justify-center my-4">
+            {" "}
+            <img src={logdivider} alt="logo" />
+          </div>
 
-      <div className='flex justify-center my-4'>  <img src={logdivider}  alt='logo' /></div>  
-
-        <div className='flex justify-center  items-center '> <div className=' p-2 h-[15%]'><button id='dice' className='text-white dicebg my-5 flex hover:bg-white hover:'  onClick={() =>fetchAdvice() } ><img src={icondice} alt="dice" /></button></div>  
-</div>        
-
- </div>
- <div className='md:hidden'>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-<br></br></div>
-
-    </div>
-    
+          <div className="flex justify-center  items-center ">
+            {" "}
+            <div className=" p-2 h-[15%]">
+              <button
+                id="dice"
+                className="text-white dicebg my-5 flex hover:bg-white hover:"
+                onClick={() => fetchAdvice()}
+              >
+                <img src={icondice} alt="dice" />
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="md:hidden">
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+        </div>
+      </div>
     </>
   );
 }
