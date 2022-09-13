df0f827f16f3fa3365544f49df9e16aa29ae8a09
Rehan Razaq <58367730+rehan509@users.noreply.github.com>
2022-09-13 16:28:00 +0500

    check verceln

--- src/App.js
+++ src/App.js
@@ -24,19 +24,19 @@ function App() {
       <div className="bg-[#1F2632] md:p-[10rem] md:h-[50rem] text-center ">
         <div className="text-white text-center pt-[10%] pb-[50%] md:pb-[0%] bg-[rgb(50,58,73)] rounded-sm">
           <h1 className="text-dice-color text-[1.25rem] title my-4 pt-5">
-            Advice ${text.id}{" "}
+            Advice $R{text.id}{" "}
           </h1>
           <h2 className=" text-[2rem] advice-text my-4">{text.advice}</h2>
 
           <div className="flex justify-center my-4">
-            {" "}
+
             <img src={logdivider} alt="logo" />
           </div>
 
           <div className="flex justify-center  items-center ">
-            {" "}
+
             <div className=" p-2 h-[15%]">
-              <button
+              <button     
                 id="dice"
                 className="text-white dicebg my-5 flex hover:bg-white hover:"
                 onClick={() => fetchAdvice()}
